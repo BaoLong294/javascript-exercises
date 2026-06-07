cc0b5a69b4b90c987c265012da409db4be939dd3
Nguyễn Bảo Long <longbao2904@gmail.com>
2026-06-07 10:34:38 +0700

    Done permutations test

--- computer_science/recursion/4_permutations/permutations.js
+++ computer_science/recursion/4_permutations/permutations.js
@@ -1,6 +1,18 @@
-const permutations = function() {
-  
+const permutations = function (array) {
+  if (array.length == 0) return [[]];
+
+  let result = [];
+
+  for (let i = 0; i < array.length; i++) {
+    const element = array[i];
+    const rest = array.filter((_, index) => index != i);
+    const perms = permutations(rest);
+    const joined = perms.map((perm) => [element].concat(perm));
+    result = result.concat(joined);
+  }
+
+  return result;
 };
-  
+
 // Do not edit below this line
 module.exports = permutations;
