14d053a5e4f347f1a2c3b8e0da2b2d3176774696
Nguyễn Bảo Long <longbao2904@gmail.com>
2026-06-07 10:30:41 +0700

    Done factorial test

--- computer_science/recursion/1_factorial/factorial.js
+++ computer_science/recursion/1_factorial/factorial.js
@@ -1,5 +1,11 @@
-const factorial = function() {
-
+const factorial = function (n) {
+  if (Number.isInteger(n)) {
+    if (n < 0) return undefined;
+    else if (n == 0) return 1;
+    else return n * factorial(n - 1);
+  } else {
+    return undefined;
+  }
 };
 
 // Do not edit below this line
