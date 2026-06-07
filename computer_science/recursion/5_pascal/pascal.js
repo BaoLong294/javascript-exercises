58d664fc1dc35bd4d5872e5f540643e65dba3295
Nguyễn Bảo Long <longbao2904@gmail.com>
2026-06-07 10:35:39 +0700

    Done pascal test

--- computer_science/recursion/5_pascal/pascal.js
+++ computer_science/recursion/5_pascal/pascal.js
@@ -1,6 +1,17 @@
-const pascal = function() {
-  
+const pascal = function (n) {
+  if (n == 1) return [1];
+
+  let result = [];
+
+  const prevRow = pascal(n - 1);
+  const paddedRow = [0].concat(prevRow).concat([0]);
+
+  for (let i = 0; i < paddedRow.length - 1; i++) {
+    result.push(paddedRow[i] + paddedRow[i + 1]);
+  }
+
+  return result;
 };
-  
+
 // Do not edit below this line
 module.exports = pascal;
