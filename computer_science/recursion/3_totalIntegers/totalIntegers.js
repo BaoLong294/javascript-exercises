696435924cf58181f92ed5f6bd747be619ba4407
Nguyễn Bảo Long <longbao2904@gmail.com>
2026-06-07 10:33:34 +0700

    Done totalIntegers test

--- computer_science/recursion/3_totalIntegers/totalIntegers.js
+++ computer_science/recursion/3_totalIntegers/totalIntegers.js
@@ -1,6 +1,16 @@
-const totalIntegers = function() {
-  
+const totalIntegers = function (input) {
+  let total = 0;
+
+  const values = Object.values(input);
+
+  for (let value of values) {
+    if (Number.isInteger(value)) total++;
+    if (Array.isArray(value) || typeof value == "object")
+      total += totalIntegers(value);
+  }
+
+  return total;
 };
-  
+
 // Do not edit below this line
 module.exports = totalIntegers;
