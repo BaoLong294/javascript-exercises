4ca135ddd68f2139f0a388344917dd5b69ae4f61
Nguyễn Bảo Long <longbao2904@gmail.com>
2026-06-07 10:32:37 +0700

    Done contains test

--- computer_science/recursion/2_contains/contains.js
+++ computer_science/recursion/2_contains/contains.js
@@ -1,6 +1,13 @@
-const contains = function() {
-  
+const contains = function (object, searchValue) {
+  const values = Object.values(object);
+
+  for (let value of values) {
+    if (value == searchValue) return true;
+    if (typeof value == "object" && contains(value, searchValue)) return true;
+  }
+
+  return false;
 };
-  
+
 // Do not edit below this line
 module.exports = contains;
